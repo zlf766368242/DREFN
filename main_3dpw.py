8dafaee4be0e0edabe1db7a7f5ad70bef6d304d6
zz jack <148221147+zlf766368242@users.noreply.github.com>
2024-11-09 16:05:33 +0800

    Add files via upload

---
+++ main_3dpw.py
@@ -0,0 +1,236 @@
+import argparse
+import torch
+from dataset.dataloader import Pose3dPW3D
+from model.MAGCNmodelGRU_3dpw_short import MultiScaleModel
+from model.MAGCNmodelGRU_3dpw_long import MultiScaleModel2
+from torch import nn, optim
+import numpy as np
+import random
+import yaml
+import os
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def lr_decay(optimizer, lr_now, gamma):
+    lr_new = lr_now * gamma
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr_new
+    return lr_new
+
+def main():
+    if args.seed >= 0:
+        seed = args.seed
+        setup_seed(seed)
+    else:
+        seed = random.randint(0,1000)
+        setup_seed(seed)
+
+    if args.debug:
+        dataset_train = Pose3dPW3D(input_n=args.past_length, output_n=args.future_length, split=0, scale=args.scale, debug=True)
+    else:
+        dataset_train = Pose3dPW3D(input_n=args.past_length, output_n=args.future_length, split=0, scale=args.scale)
+   
+    loader_train = torch.utils.data.DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, drop_last=True,
+                                               num_workers=8)
+
+    dataset_test = Pose3dPW3D(input_n=args.past_length, output_n=args.future_length, split=2, scale=args.scale)
+    loaders_test = torch.utils.data.DataLoader(dataset_test, batch_size=128, shuffle=False, drop_last=False,
+                                                num_workers=8)
+
+
+    dim_used = dataset_train.dim_used
+    if args.task == 'short':
+        model = MultiScaleModel(
+        in_dim=3,
+        h_dim=args.nf,
+        past_timestep=args.past_length,
+        future_timestep=args.future_length,
+        kernel_size=args.kernel_size,
+        graph_args_j=args.graph_args_j,
+        graph_args_b=args.graph_args_b)
+    else:
+        model = MultiScaleModel2(
+        in_dim=3,
+        h_dim=args.nf,
+        past_timestep=args.past_length,
+        future_timestep=args.future_length,
+        kernel_size=args.kernel_size,
+        graph_args_j=args.graph_args_j,
+        graph_args_b=args.graph_args_b)
+    model = model.cuda()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    best_test_loss = 1e8
+    best_epoch = 0
+    lr_now = args.lr
+
+    for epoch in range(0, args.epochs):
+        if args.apply_decay:
+            if epoch % args.epoch_decay == 0 and epoch > 0:
+                lr_now = lr_decay(optimizer, lr_now, args.lr_gamma)
+        train(model, optimizer, epoch, loader_train, dim_used)
+        if epoch % args.test_interval == 0:
+            mpjpe = test(model, optimizer, epoch, ('all', loaders_test), dim_used, backprop=False)
+            avg_mpjpe = mpjpe
+            print('avg mpjpe:',avg_mpjpe)
+            avg_avg_mpjpe = np.mean(avg_mpjpe)
+
+            if avg_avg_mpjpe < best_test_loss:
+                best_test_loss = avg_avg_mpjpe
+                best_all_test_loss = avg_mpjpe
+                best_epoch = epoch
+                state = {'epoch': epoch,
+                            'state_dict': model.state_dict(),
+                            'optimizer': optimizer.state_dict()}
+                if args.model_save_name == 'default':
+                    if args.future_length == 30 or args.future_length == 10:
+                        file_path = os.path.join(args.model_save_dir, '3dpw_ckpt_long_best.pth')
+                    else:
+                        file_path = os.path.join(args.model_save_dir, '3dpw_ckpt_best.pth')
+                else:
+                    if args.future_length == 30 or args.future_length == 10:
+                        file_path = os.path.join(args.model_save_dir, args.model_save_name+'3dpw_ckpt_long_best.pth')
+                    else:
+                        file_path = os.path.join(args.model_save_dir, args.model_save_name+'3dpw_ckpt_best.pth')
+                torch.save(state, file_path)
+
+            state = {'epoch': epoch,
+                            'state_dict': model.state_dict(),
+                            'optimizer': optimizer.state_dict()}
+            if args.model_save_name == 'default':
+                if args.future_length == 30 or args.future_length == 10:
+                    file_path = os.path.join(args.model_save_dir, '3dpw_ckpt_long_'+str(epoch)+str(avg_avg_mpjpe)+'.pth')
+                else:
+                    file_path = os.path.join(args.model_save_dir, '3dpw_ckpt_'+str(epoch)+str(avg_avg_mpjpe)+'.pth')
+            else:
+                if args.future_length == 30 or args.future_length == 10:
+                    file_path = os.path.join(args.model_save_dir, args.model_save_name+'3dpw_ckpt_long_'+str(epoch)+str(avg_avg_mpjpe)+'.pth')
+                else:
+                    file_path = os.path.join(args.model_save_dir, args.model_save_name+'3dpw_ckpt_'+str(epoch)+str(avg_avg_mpjpe)+'.pth')
+            torch.save(state, file_path)
+
+            print("Best Test Loss: %.5f \t Best epoch %d" % (best_test_loss, best_epoch))
+            print("Best AVG loss:",best_all_test_loss)
+            print('The seed is :',seed)
+
+    return
+
+def train(model, optimizer, epoch, loader, dim_used=[], backprop=True, x=0.4):
+    if backprop:
+        model.train()
+    else:
+        model.eval()
+
+    res = {'epoch': epoch, 'loss': 0, 'counter': 0}
+
+    for batch_idx, data in enumerate(loader):
+        batch_size, n_nodes, length, _ = data[0].size()
+        data = [d.to(device) for d in data]
+        loc, vel, loc_end, _, item = data
+        optimizer.zero_grad()
+        all_traj = torch.cat([loc,loc_end],dim=2)
+        all_traj = all_traj.float()
+
+        loc_pred, denoised_pred = model(all_traj)
+        loss1 = torch.mean(torch.norm(loc_pred - loc_end, dim=-1))
+
+        if args.denoise_mode == 'all':
+            loss2 = torch.mean(torch.norm(denoised_pred - all_traj, dim=-1))
+        elif args.denoise_mode == 'past':
+            loss2 = torch.mean(torch.norm(denoised_pred - loc, dim=-1))
+        elif args.denoise_mode == 'future':
+            loss2 = torch.mean(torch.norm(denoised_pred - loc_end, dim=-1))
+        else:
+            raise ValueError("args.denoise_mode")
+        loss = x * loss1 + (2 - x) * loss2
+
+        if backprop:
+            loss.backward()
+            optimizer.step()
+        res['loss'] += loss.item()*batch_size
+        res['counter'] += batch_size
+
+    print('%s epoch %d avg loss: %.5f' % ('train', epoch, res['loss'] / res['counter']))
+
+    return res['loss'] / res['counter']
+
+def test(model, optimizer, epoch, act_loader,dim_used=[],backprop=False):
+    act, loader = act_loader[0], act_loader[1]
+    model.eval()
+    res = {'epoch': epoch, 'loss': 0, 'coord_reg': 0, 'counter': 0, 'ade': 0}
+    output_n = args.future_length
+    if output_n == 12:
+        eval_frame = [2, 5, 8, 11]
+    elif output_n == 30:
+        eval_frame = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29]
+    elif output_n == 10:
+        eval_frame = [5, 7, 9]
+
+    t_3d = np.zeros(len(eval_frame))
+
+    with torch.no_grad():
+        for batch_idx, data in enumerate(loader):
+            batch_size, n_nodes, length, _ = data[0].size()
+            data = [d.to(device) for d in data]
+            loc, vel, loc_end, loc_end_ori,_ = data
+            pred_length = loc_end.shape[2]
+
+            optimizer.zero_grad()
+
+            loc_end_fake = torch.zeros_like(loc_end)
+            all_traj = torch.cat([loc,loc_end_fake],dim=2)
+            all_traj = all_traj.float()
+
+            loc_pred = model.predict(all_traj)
+
+            loc_end_ori = loc_end_ori.float()
+            pred_3d = loc_end_ori.clone()
+            loc_pred = loc_pred.transpose(1,2)
+            loc_pred = loc_pred.contiguous().view(batch_size,loc_end.shape[2],n_nodes*3)
+
+            pred_3d[:,:,dim_used] = loc_pred
+
+            pred_p3d = pred_3d.contiguous().view(batch_size, pred_length, -1, 3)
+            targ_p3d = loc_end_ori.contiguous().view(batch_size, pred_length, -1, 3)
+ 
+            for k in np.arange(0, len(eval_frame)):
+                j = eval_frame[k]
+                t_3d[k] += torch.mean(torch.norm(targ_p3d[:, j, :, :].contiguous().view(-1, 3) - pred_p3d[:, j, :, :].contiguous().view(-1, 3), 2, 1)).item() * batch_size
+            
+            res['counter'] += batch_size
+
+    t_3d *= args.scale
+    N = res['counter']
+    return t_3d / N
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='VAE MNIST Example')
+    parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=-1, metavar='S', help='random seed (default: -1)')
+    parser.add_argument("--debug",action='store_true')
+    parser.add_argument('--model_save_dir', type=str, default='ckpt', help='dir to save model')
+    parser.add_argument("--model_save_name",type=str,default="default")
+    parser.add_argument("--task",type=str,default="short")
+    args = parser.parse_args()
+
+    if args.task == 'short':
+        with open('cfg/3dpw_short.yml', 'r') as f:
+            yml_arg = yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        with open('cfg/3dpw_long.yml', 'r') as f:
+            yml_arg = yaml.load(f, Loader=yaml.FullLoader)
+
+    parser.set_defaults(**yml_arg)
+    args = parser.parse_args()
+    args.cuda = True
+
+    device = torch.device("cuda" if args.cuda else "cpu")
+    main()
+
+
+
+
---
+++ test_cmu.py
@@ -0,0 +1,240 @@
+import argparse
+import torch
+from dataset.dataloader import CMU_Motion3D
+from model.MAGCNmodelGRU_cmu_short import MultiScaleModel
+from model.MAGCNmodelGRU_cmu_long import MultiScaleModel2
+from torch import nn, optim
+import numpy as np
+import random
+import yaml
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def lr_decay(optimizer, lr_now, gamma):
+    lr_new = lr_now * gamma
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr_new
+    return lr_new
+
+def main():
+    if args.seed >= 0:
+        seed = args.seed
+        setup_seed(seed)
+    else:
+        seed = random.randint(0,1000)
+        setup_seed(seed)
+    print('The seed is :',seed)
+
+    if args.debug:
+        dataset_train = CMU_Motion3D(actions='walking', input_n=args.past_length, output_n=args.future_length, split=0, scale=args.scale)
+    else:
+        dataset_train = CMU_Motion3D(actions='all', input_n=args.past_length, output_n=args.future_length, split=0, scale=args.scale)
+        
+    acts = ["basketball", "basketball_signal", "directing_traffic", "jumping", "running", "soccer", "walking",
+               "washwindow"]
+    loaders_test = {}
+    for act in acts:
+        dataset_test = CMU_Motion3D(actions=act, input_n=args.past_length, output_n=args.future_length, split=1, scale=args.scale,dim_used=dataset_train.dim_used)
+        loaders_test[act] = torch.utils.data.DataLoader(dataset_test, batch_size=args.batch_size, shuffle=False, drop_last=False,
+                                                num_workers=8)
+
+    dim_used = dataset_test.dim_used
+    if args.task == 'short':
+        model = MultiScaleModel(
+        in_dim=3,
+        h_dim=args.nf,
+        past_timestep=args.past_length,
+        future_timestep=args.future_length,
+        kernel_size=args.kernel_size,
+        graph_args_j=args.graph_args_j,
+        graph_args_b=args.graph_args_b)
+       
+        weights_path_j = '/root/dataDisk/DREFN/pretrain/cmu_short_model_cmu_ckpt_1.pth'
+    else:
+        model = MultiScaleModel2(
+        in_dim=3,
+        h_dim=args.nf,
+        past_timestep=args.past_length,
+        future_timestep=args.future_length,
+        kernel_size=args.kernel_size,
+        graph_args_j=args.graph_args_j,
+        graph_args_b=args.graph_args_b)
+        weights_path_j = '/root/dataDisk/DREFN/pretrain/cmu_long_model_cmu_ckpt_1.pth'
+    model = model.cuda()
+    checkpoint = torch.load(weights_path_j)
+    if 'state_dict' in checkpoint:
+        pretrained_weights_j = checkpoint['state_dict']
+    else:
+        pretrained_weights_j = checkpoint
+    pretrained_weights_j = {k: v for k, v in pretrained_weights_j.items() if k in model.state_dict()}
+    model.load_state_dict(pretrained_weights_j)
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    model.eval()
+
+    if args.future_length == 25 or args.future_length == 13:
+        avg_mpjpe = np.zeros((2))
+    elif args.future_length == 15:
+        avg_mpjpe = np.zeros((2))
+    else:
+        avg_mpjpe = np.zeros((4))
+    for act in acts:
+        mpjpe = test(model, optimizer, 0, (act, loaders_test[act]), dim_used, backprop=False)
+        avg_mpjpe += mpjpe
+    avg_mpjpe = avg_mpjpe / len(acts)
+    avg_mpjpe = np.floor(avg_mpjpe * 10) / 10
+    avg_mpjpe = np.array2string(avg_mpjpe, formatter={'float_kind': lambda x: "%.1f" % x})
+    print('avg mpjpe:', avg_mpjpe)
+
+    return
+
+def train(model, optimizer, epoch, loader, dim_used=[], backprop=True, x=0.4):
+    if backprop:
+        model.train()
+    else:
+        model.eval()
+
+    res = {'epoch': epoch, 'loss': 0, 'counter': 0}
+
+    for batch_idx, data in enumerate(loader):
+        batch_size, n_nodes, length, _ = data[0].size()
+        data = [d.to(device) for d in data]
+        loc, vel, loc_end, _, item = data
+
+        optimizer.zero_grad()
+        if args.time_exp:
+            torch.cuda.synchronize()
+        all_traj = torch.cat([loc, loc_end], dim=2)
+
+        loc_pred, denoised_pred = model(all_traj)
+
+        loss1 = torch.mean(torch.norm(loc_pred - loc_end, dim=-1))
+
+        if args.denoise_mode == 'all':
+            loss2 = torch.mean(torch.norm(denoised_pred - all_traj, dim=-1))
+        elif args.denoise_mode == 'past':
+            loss2 = torch.mean(torch.norm(denoised_pred - loc, dim=-1))
+        elif args.denoise_mode == 'future':
+            loss2 = torch.mean(torch.norm(denoised_pred - loc_end, dim=-1))
+        else:
+            raise ValueError("args.denoise_mode")
+        loss = x * loss1 + (2 - x) * loss2
+
+        if backprop:
+            loss.backward()
+            optimizer.step()
+        res['loss'] += loss.item() * batch_size
+        res['counter'] += batch_size
+
+    print('%s epoch %d avg loss: %.5f' % ('train', epoch, res['loss'] / res['counter']))
+
+    return res['loss'] / res['counter']
+
+def test(model, optimizer, epoch, act_loader, dim_used=[], backprop=False):
+    act, loader = act_loader[0], act_loader[1]
+
+    model.eval()
+
+    validate_reasoning = False
+    if validate_reasoning:
+        acc_list = [0] * args.n_layers
+    res = {'epoch': epoch, 'loss': 0, 'counter': 0}
+
+    output_n = args.future_length
+    if output_n == 25:
+        eval_frame = [13, 24]
+    elif output_n == 15:
+        eval_frame = [3, 14]
+    elif output_n == 10:
+        eval_frame = [1, 3, 7, 9]
+    elif output_n == 13:
+        eval_frame = [6, 12]
+    t_3d = np.zeros(len(eval_frame))
+
+    with torch.no_grad():
+        for batch_idx, data in enumerate(loader):
+            batch_size, n_nodes, length, _ = data[0].size()
+            data = [d.to(device) for d in data]
+            loc, vel, loc_end, loc_end_ori, _ = data
+            loc_start = loc[:, :, -1:]
+            pred_length = loc_end.shape[2]
+
+            optimizer.zero_grad()
+
+            loc_end_fake = torch.zeros_like(loc_end)
+            all_traj = torch.cat([loc, loc_end_fake], dim=2)
+            loc_pred = model.predict(all_traj)
+
+            pred_3d = loc_end_ori.clone()
+            loc_pred = loc_pred.transpose(1, 2)
+            loc_pred = loc_pred.contiguous().view(batch_size, loc_end.shape[2], n_nodes * 3)
+
+            joint_to_ignore = np.array([16, 20, 29, 24, 27, 33, 36])
+            index_to_ignore = np.concatenate((joint_to_ignore * 3, joint_to_ignore * 3 + 1, joint_to_ignore * 3 + 2))
+            joint_equal = np.array([15, 15, 15, 23, 23, 32, 32])
+            index_to_equal = np.concatenate((joint_equal * 3, joint_equal * 3 + 1, joint_equal * 3 + 2))
+
+            pred_3d[:, :, dim_used] = loc_pred
+            pred_3d[:, :, index_to_ignore] = pred_3d[:, :, index_to_equal]
+            pred_p3d = pred_3d.contiguous().view(batch_size, pred_length, -1, 3)  # [:, input_n:, :, :]
+            targ_p3d = loc_end_ori.contiguous().view(batch_size, pred_length, -1, 3)  # [:, input_n:, :, :]
+
+            for k in np.arange(0, len(eval_frame)):
+                j = eval_frame[k]
+                t_3d[k] += torch.mean(torch.norm(
+                    targ_p3d[:, j, :, :].contiguous().view(-1, 3) - pred_p3d[:, j, :, :].contiguous().view(-1, 3), 2,
+                    1)).item() * batch_size
+
+            res['counter'] += batch_size
+
+    t_3d *= args.scale
+    N = res['counter']
+    actname = "{0: <14} |".format(act)
+    if args.future_length == 25 or args.future_length == 13:
+        print('Act: {},  ErrT: {:.3f} {:.3f} , TestError {:.4f}' \
+              .format(actname,
+                      float(t_3d[0]) / N, float(t_3d[1]) / N,
+                      float(t_3d.mean()) / N))
+    elif args.future_length == 15:
+        print('Act: {},  ErrT: {:.3f} {:.3f}, TestError {:.4f}' \
+              .format(actname,
+                      float(t_3d[0]) / N, float(t_3d[1]) / N,
+                      float(t_3d.mean()) / N))
+    else:
+        print('Act: {},  ErrT: {:.3f} {:.3f} {:.3f} {:.3f}, TestError {:.4f}' \
+              .format(actname,
+                      float(t_3d[0]) / N, float(t_3d[1]) / N, float(t_3d[2]) / N, float(t_3d[3]) / N,
+                      float(t_3d.mean()) / N))
+
+    return t_3d / N
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='VAE MNIST Example')
+    parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=-1, metavar='S', help='random seed (default: -1)')
+    parser.add_argument("--debug", action='store_true')
+    parser.add_argument('--model_save_dir', type=str, default='ckpt', help='dir to save model')
+    parser.add_argument("--model_save_name", type=str, default="default")
+    parser.add_argument("--task", type=str, default="short")
+    args = parser.parse_args()
+
+    if args.task == 'short':
+        with open('cfg/cmu_short.yml', 'r') as f:
+            yml_arg = yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        with open('cfg/cmu_long.yml', 'r') as f:
+            yml_arg = yaml.load(f, Loader=yaml.FullLoader)
+
+    parser.set_defaults(**yml_arg)
+    args = parser.parse_args()
+    args.cuda = True
+
+    device = torch.device("cuda" if args.cuda else "cpu")
+    main()
+
